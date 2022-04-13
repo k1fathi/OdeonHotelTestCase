f8b589f54fdb6574712febd43c3f6a9488107e78
k1fathi <javad.fathi@sarente.com>
2022-04-13 13:03:47 +0300

    DB schema with dummy data included

--- DBScripts/HotelsSeeder.sql
+++
@@ -1,6 +0,0 @@
-﻿USE [OdeonHotel]
-GO
-
-INSERT INTO [dbo].[Hotels] SELECT NEWID(),[Name],GETDATE(),NULL FROM [OdeonHotel].[dbo].[HotelsName] 
-GO
-SELECT * FROM [dbo].[Hotels];
---
+++ DBScripts/dbo/Tables/RoomTypes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[RoomTypes](
+	[Id] [uniqueidentifier] NOT NULL,
+	[RoomTypeName] [nvarchar](max) NOT NULL,
+	[CreateDate] [datetime2](7) NOT NULL,
+	[UpdateDate] [datetime2](7) NULL,
+ CONSTRAINT [PK_RoomTypes] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
